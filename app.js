d1a7d1f8a41ee6039aed2e2213d50994fd4b72ef
nuninfarid <nula@hacktiv8.ac.id>
2024-08-13 09:38:35 +0700

    merge from development

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const router = require("./routes");
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/", router);
 
 app.listen(PORT, () => {
-  console.log("jalan brok!");
+  console.log("Oke");
 });
